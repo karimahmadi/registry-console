7e5e7903936a0226832e4c4685cdb05ba470f98b
Morten Høfft <mhoefft@gbif.org>
2019-01-17 13:48:01 +0100

    changed to match doc specs

--- tmp_vocabulary_api/index.js
+++ tmp_vocabulary_api/index.js
@@ -6,21 +6,52 @@ const FileSync = require("lowdb/adapters/FileSync");
 const app = express();
 const adapter = new FileSync("db.json");
 const db = low(adapter);
+const uuidv1 = require('uuid/v1');
 
 app.use(bodyParser.json());
 
 // Set some defaults
-// db.defaults({ vocab: [{
-// 	key: 'BASIS_OF_RECORD',
-// 	title: {
-// 		en: 'Basis of record',
-// 		da: 'Evidens'
-// 	},
-// 	description: {
-// 		en: 'What is the type of evidence for the record',
-// 		da: 'hvilken type evidens er der for observationen'
-// 	}
-// }] }).write();
+db.defaults({
+  vocab: [{
+    key: uuidv1(),
+    namespace: 'gbif.org',
+    name: 'BasisOfRecord',
+    label: {
+      eng: 'Basis of record',
+      dan: 'Evidens'
+    },
+    definition: {
+      eng: 'What is the type of evidence for the record',
+      dan: 'hvilken type evidens er der for observationen'
+    },
+    externalDefinitionUrl: ['https://github.com/tdwg/website/issues/128'],
+    editorialNotes: ['#lorem ipsum\nsomething goes here']
+  }],
+  terms: [
+    {
+      key: uuidv1(),
+      vocabularyKey: "234",
+      parentKey: undefined,
+      replacedByKey: undefined,
+      name: 'HumanObservation',
+      label: {
+        eng: 'Human observation',
+        dan: 'Menneskelig observation'
+      },
+      definition: {
+        eng: 'A human being observed the species at the time and place it occurred and recorded it.',
+      },
+      alternativeLabel: {
+        dan: ['msk observation'],
+        shared: ['Human obs']
+      },
+      misspeltLabel: {
+        dan: ['mensklig observation'],
+        shared: ['Humane observatoin']
+      }
+    }
+]
+}).write();
 
 // Add a post
 // db.get("posts")
@@ -30,26 +61,19 @@ app.use(bodyParser.json());
 // Set a user using Lodash shorthand syntax
 //db.set("user.name", "typicode").write();
 
-app.get("/vocabulary/:key", function(req, res) {
-  const locales = req.query.locales ? req.query.locales.split(",") : ["en"];
+app.get("/vocabulary/:key", function (req, res) {
   const vocab = db
     .get("vocab")
-    .find({ key: req.params.key })
-    .cloneDeep()
+    .find({ name: req.params.key })
     .value();
-
   if (vocab) {
-    vocab.title = _.pickBy(vocab.title, (v, k) => locales.includes(k));
-    vocab.description = _.pickBy(vocab.description, (v, k) =>
-      locales.includes(k)
-    );
-    res.json(vocab);
+    res.json(filterLocales(vocab, req.query.locales));
   } else {
     res.sendStatus(404);
   }
 });
 
-app.get("/vocabulary", function(req, res) {
+app.get("/vocabulary", function (req, res) {
   const q = (req.query.q || "").toLowerCase();
   const offset = _.toNumber(req.query.offset || 0);
   const limit = _.toNumber(req.query.limit || 5);
@@ -57,8 +81,8 @@ app.get("/vocabulary", function(req, res) {
     .get("vocab")
     .filter(item => {
       return (
-        _.includes(item.key.toLowerCase(), q) ||
-        _.includes(item.title.en.toLowerCase(), q)
+        _.includes(item.name.toLowerCase(), q) ||
+        _.includes(_.get(item, 'label.eng', '').toLowerCase(), q)
       );
     })
     .cloneDeep()
@@ -68,15 +92,25 @@ app.get("/vocabulary", function(req, res) {
     count: results.length,
     offset: offset,
     limit: limit,
-    results: results.slice(offset, limit)
+    results: _.map(results.slice(offset, limit), x => filterLocales(x, req.query.locales))
   });
 });
 
-app.post("/vocabulary", function(req, res) {
+app.post("/vocabulary", function (req, res) {
   db.get("vocab")
-		.push(req.body)
-		.write()
-	res.json(req.body);
+    .push(req.body)
+    .write()
+  res.json(req.body);
 });
+
+function filterLocales(obj, locales) {
+  let item = _.cloneDeep(obj);
+  locales = locales ? locales.split(",") : ["eng"];
+  item.label = _.pickBy(item.label, (v, k) => locales.includes(k));
+  item.definition = _.pickBy(item.definition, (v, k) =>
+    locales.includes(k)
+  );
+  return item;
+}
 
 app.listen(3000);
